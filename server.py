2c31167639375ca7fd98390f8311536952f8aa57
Amanda Barr <amanda.j.webster86@gmail.com>
2020-09-01 23:25:28 -0700

    Update server.py
    Removed duplicated code from pre-React app.

--- server.py
+++ server.py
@@ -81,17 +81,6 @@ def check_user_login_setting():
     else:
         return False
 
-
-# @app.route("/")
-# def fetch_home():
-#     """View Homepage"""
-
-#     if "user" in session:
-#         username = session["user"]
-#         return render_template("homepage.html")
-#     else:
-#         return redirect("/login")
-
 @app.route("/stories")
 def fetch_stories():
     """Return news stories as JSON"""
@@ -203,42 +192,7 @@ def story_db_to_json(story, favorite):
 
     return jsonify({})
 
-# @app.route("/save_article", methods = ["GET", "POST"])
-# def save_article_to_favorites():
-#     # crud function to save article under specific user.  Need to get primary keys user_id and story_id and option to comment/tag
-#     story_id = request.form.get("storyId")
-#     # session["user_id"]
 
-#     crud.save_story(session["user_id"], story_id)
-
-#     return jsonify({"success": True})
-
-    # return jsonify({"response": "ok"})
-    # on front end, event listener or re-use existing, test that event listener is working by printing, make network request, check that
-    # server is getting response, form.get vs params,
-
-
-# @app.route("/login", methods = ["GET", "POST"])
-# def user_login():
-#     """Allow user to log in or option to create new account"""
-
-#     username = request.form.get("username")
-
-#     password = request.form.get("password")
-
-#     user = crud.get_user(username, password)
-
-
-#     if user:
-#         session["user"] = username
-#         session["password"] = password
-#         session["user_id"] = user.user_id
-#         print(session)
-#         print(session['user_id'])
-#         return redirect("/")
-#     else:
-#         flash("Please enter a valid username and password")
-#         return render_template("login.html")
 
 # @app.route("/api/user", methods = ["GET"])
 # def user_api():
@@ -266,45 +220,6 @@ def create_account():
         crud.create_user(email, password)
         flash('Account created! Please log in.')
     pass
-
-# @app.route("/profile")
-# def show_user_profile():
-
-#     return render_template("profile.html")
-
-
-# @app.route("/profile_stories")
-# def user_profile_data():
-#     """View user's profile and saved articles"""
-
-#     user_id = session["user_id"]
-
-#     profile_stories = crud.get_saved_stories_by_user(user_id)
-#     print(profile_stories)
-
-#     story_list = [story_db_to_json(story, True) for story in profile_stories]
-
-#     return jsonify(story_list)
-
-# def story_db_to_json(story, favorite):
-
-#     return {
-#             "author": story.author,
-#             "content": story.content,
-#             "description": story.description,
-#             "favorite": favorite,
-#             "publishedAt": story.published,
-#             "source": {
-#                 "id": None,
-#                 "name": story.source
-#             },
-#             "storyId": story.story_id,
-#             "title": story.title,
-#             "url": story.story_link,
-#             "urlToImage": story.image
-#             }
-
-#     return jsonify({})
 
 
 
